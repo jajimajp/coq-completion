5c958a02bd9897dcad40def16bae26282233eb45
Yajima Soichi <jajima.jp@gmail.com>
2024-02-20 16:17:56 +0900

    Update config

--- plugin/compl.mli
+++ plugin/compl.mli
@@ -1,5 +1,7 @@
+(** Manage completion algorithm state *)
+
 val complete :
-  axioms: Libnames.qualid list -> (* 公理名のリスト *)
-    hint_db_name: string -> (* 完備化された定理が登録されるヒントDBの名前 *)
-      ops: Libnames.qualid list (* 関数名のリスト 引数0個の定数も含む *) ->
-        string list (* 出力のリスト *)
+  axioms:Libnames.qualid list ->
+  constants:Libnames.qualid list ->
+  hint_db_name:string ->
+  Pp.t
