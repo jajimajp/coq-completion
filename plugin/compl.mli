885b4234dd7aaae07b66658eb61ddaa1f835fb49
Yajima Soichi <jajima.jp@gmail.com>
2024-02-20 16:20:25 +0900

    YAGNI

--- plugin/compl.mli
+++
@@ -1,7 +0,0 @@
-(** Manage completion algorithm state *)
-
-val complete :
-  axioms:Libnames.qualid list ->
-  constants:Libnames.qualid list ->
-  hint_db_name:string ->
-  Pp.t
--- plugin/tomaparser.mli
+++ plugin/tomaparser.mli
@@ -1,37 +1,3 @@
-module Completion : sig
-  type ruleid
-  type rule = ruleid * Equation.t
-
-  (** Strategies to prove rules. *)
-  type proof_strat =
-     (* Immediately proved by one of axioms *)
-    | ByAxiom
-     (* By critial-pair of [rule1] and [rule2] with superposition [term] *)
-    | CriticalPairs of rule * rule * Equation.Term.t
-     (* Reduce rebundant rule([ruleid]) by rules *)
-    | Reduction of ruleid * ruleid list
-
-  (** Algorithm state *)
-  type state
-
-  val init :
-    constants:string list -> axioms:rule list -> state
-
-  (** Possible actions in completion steps. *)
-  type action =
-     (* Prove rule with strategy [proof_strat] *)
-    | Prove of rule * proof_strat
-     (* Terminate steps with completed [rules] *)
-    | Terminate of ruleid list
-     (* Failure of steps with reason [string] *)
-    | Fail of string
-
-  val execute_proof : state -> rule -> proof_strat -> state
-end
-
-type proof_action =
-  Completion.ruleid * Equation.t * Completion.proof_strat
-
 open My_term
 
 type termid = string
