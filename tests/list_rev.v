f1a6e62ba69f84263ffd260bc3261669fbadf4f4
Yajima Soichi <jajima.jp@gmail.com>
2024-03-11 01:39:53 +0900

    Check toma version

---
+++ tests/list_rev.v
@@ -0,0 +1,16 @@
+Require Import Coq.Setoids.Setoid.
+Require Import Loader.
+
+Variable G : Set.
+    
+Variable cons : G -> G -> G.
+Variable last : G -> G -> G.
+Variable nil : G.
+Variable rev : G -> G.
+Axiom ax0 : forall x y l, last x (cons y l) = last y l.
+Axiom ax1 : forall x, last x nil = x.
+Axiom ax2 : forall x l, rev (cons x l) = cons (last x l) (rev l).
+Axiom ax3 : rev nil = nil.
+Create HintDb hint_compl.
+Complete ax0 ax1 ax2 ax3 : cons last nil rev : hint_compl.
+Print Rewrite HintDb hint_compl.
