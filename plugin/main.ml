f27044d5347575ccb495af86041c4f672a733bef
Yajima <119467077+jajimajp@users.noreply.github.com>
2024-04-22 17:00:44 +0900

    ORELSE は ELSE を評価してしまうため、 IFCATCH に変更して遅延させる

--- plugin/main.ml
+++ plugin/main.ml
@@ -11,18 +11,21 @@ let tac_prove_by_axiom ~(axioms : Libnames.qualid list) =
   let tactic_of axiom use_symmetry =
     Tactics.intros <*>
     (if use_symmetry then Tactics.symmetry else Tacticals.tclIDTAC) <*>
-    Tactics.apply (EConstr.mkRef (Nametab.global axiom, EConstr.EInstance.empty)) in
+    Tactics.apply (EConstr.mkRef (Nametab.global axiom, EConstr.EInstance.empty))
+  in
   let rec aux axioms use_symmetry =
-    match axioms, use_symmetry with
-    | hd :: tl, false ->
-      Tacticals.tclORELSE
-        (tactic_of hd use_symmetry)
-        (aux axioms true)
-    | hd :: tl, true ->
-      Tacticals.tclORELSE
-        (tactic_of hd use_symmetry)
-        (aux tl false)
-    | [], _ -> Tacticals.tclFAIL (Pp.str "Could not prove goal by axioms.") in
+    match axioms with
+    | [] -> Tacticals.tclFAIL (Pp.str "Could not prove goal by axioms.")
+    | hd :: tl ->
+      Tacticals.tclIFCATCH
+        (Tacticals.tclPROGRESS (tactic_of hd use_symmetry))
+        (fun _ -> Tacticals.tclIDTAC)
+        (fun _ ->
+          match use_symmetry with
+          | false ->
+            aux axioms true
+          | true ->
+            aux tl false) in
   aux axioms false
 
 (** 公理のうちのどれかから直ちに導ける規則を証明する
@@ -74,37 +77,9 @@ let add_local_rewrite_hint (base: string) (lcsr: Constrexpr.constr_expr) : Pp.t 
   let _ = add_hints base in
   Pp.strbrk "Added rewrite hints."
 
-(*
-  critical_pair による定理の証明
-  name: 定理の名前
-  n1: 証明に使う１つ目の定理名
-  n2: 証明に使う２つ目の定理名
-  l: 定理の左辺
-  r: 定理の右辺
-  crit: critical pair
-*)
-let proof_using_crit ~name ~n1 ~n2 ~l ~r ~crit ~(constants:constants option) =
-  let constants = begin match constants with
-  | None -> My_term.default_constants
-  | Some cs -> cs
-  end in
-  let e = My_term.to_constrexpr_raw (l, r) constants in
-  let evars = My_term.variables_except_constants (l, r) constants in
-  let e1 = My_term.to_constrexpr_raw (crit, l) constants in
+let tac_prove_by_crit ~evars ~constants ~e1 ~e2 ~r1 ~r2 ~crit ~l ~r =
   let e1vars = My_term.variables_except_constants (crit, l) constants in
-  let e2 = My_term.to_constrexpr_raw (crit, r) constants in
   let e2vars = My_term.variables_except_constants (crit, r) constants in
-  let r1 = Libnames.qualid_of_string n1 in
-  let r2 = Libnames.qualid_of_string n2 in
-  let env = Global.env () in 
-  let sigma = Evd.from_env env in
-  let (sigma, body) = Constrintern.interp_constr_evars env sigma e in
-  let (sigma, e1) = Constrintern.interp_constr_evars env sigma e1 in
-  let (sigma, e2) = Constrintern.interp_constr_evars env sigma e2 in
-  let typ = EConstr.to_constr sigma body in
-  let info = Declare.Info.make ~poly:false () in
-  let cinfo = Declare.CInfo.make ~name ~typ () in
-  let (t, types, ustate, obl_info) = Declare.Obls.prepare_obligation ~name ~types:None ~body sigma in
   let open Proofview.Notations in
   let open Equality in
   let open Tactypes in
@@ -129,7 +104,7 @@ let proof_using_crit ~name ~n1 ~n2 ~l ~r ~crit ~(constants:constants option) =
   let rewriteRL_with_binds c (vars : string list) =
     let binds = ExplicitBindings (List.map explicit_bind vars) in
     general_rewrite ~where:None ~l2r:false (OnlyOccurrences [1]) ~freeze:true ~dep:true ~with_evars:false (c, binds) in
-  let gen_tactic at1 at2 = Tactics.assert_by Names.Name.Anonymous e1 (
+  let tactic_of at1 at2 = Tactics.assert_by Names.Name.Anonymous e1 (
       Tactics.intros
       <*> rewriteLR (fun env sigma -> sigma, ((EConstr.mkRef (Nametab.global r1, EConstr.EInstance.empty)), NoBindings)) at1
       <*> Tactics.reflexivity)
@@ -141,28 +116,56 @@ let proof_using_crit ~name ~n1 ~n2 ~l ~r ~crit ~(constants:constants option) =
     <*> rewriteRL_with_binds (EConstr.mkVar (Names.Id.of_string "H")) e1vars
     <*> rewriteLR_with_binds (EConstr.mkVar (Names.Id.of_string "H0")) e2vars
     <*> Tactics.reflexivity in
-  let rec try_proof (at1, at2) =
-    (* 自然数のペアに対して、次のペアを返す関数。 (1, 1)から始めると全てのペアを列挙する。 *)
-    (* 例: (1, 1), (1, 2), (2, 2), (2, 1), (1, 3), ... *)
-    let next_pair (x, y) =
-      if y = 1 then
-        (1, x + 1)
-      else if x < y then (* 対角線より左側にある点 *)
-        (x + 1, y)
-      else 
-        (x, y - 1)
-    in
-    let tactic = gen_tactic at1 at2 in
-    let tactic = Proofview.tclORELSE tactic (fun _ -> Tacticals.tclIDTAC) in
-    let _, progress = Declare.Obls.add_definition ~pm:(Declare.OblState.empty) ~cinfo ~info ~uctx:ustate ~tactic obl_info in
-    begin match progress with
-    | Remain _ -> try_proof (next_pair (at1, at2))
-    | Dependent -> try_proof (next_pair (at1, at2))
-    | Defined _ -> ()
-    end
-    in
-  let () = try_proof (1, 1) in
-    Pp.strbrk "Proof"
+  (* 自然数のペアに対して、次のペアを返す関数。 (1, 1)から始めると全てのペアを列挙する。 *)
+  (* 例: (1, 1), (1, 2), (2, 2), (2, 1), (1, 3), ... *)
+  let next_pair (x, y) =
+    if y = 1 then
+      (1, x + 1)
+    else if x < y then (* 対角線より左側にある点 *)
+      (x + 1, y)
+    else 
+      (x, y - 1) in
+  let rec aux (at1, at2) =
+    Tacticals.tclIFCATCH
+      (Tacticals.tclPROGRESS (tactic_of at1 at2))
+      (fun _ -> Tacticals.tclIDTAC)
+      (fun _ -> aux (next_pair (at1, at2))) in
+  aux (1, 1)
+
+(*
+  critical_pair による定理の証明
+  name: 定理の名前
+  n1: 証明に使う１つ目の定理名
+  n2: 証明に使う２つ目の定理名
+  l: 定理の左辺
+  r: 定理の右辺
+  crit: critical pair
+*)
+let proof_using_crit ~name ~n1 ~n2 ~l ~r ~crit ~(constants:constants option) =
+  let constants = begin match constants with
+  | None -> My_term.default_constants
+  | Some cs -> cs
+  end in
+  let e = My_term.to_constrexpr_raw (l, r) constants in
+  let evars = My_term.variables_except_constants (l, r) constants in
+  let e1 = My_term.to_constrexpr_raw (crit, l) constants in
+  let e2 = My_term.to_constrexpr_raw (crit, r) constants in
+  let r1 = Libnames.qualid_of_string n1 in
+  let r2 = Libnames.qualid_of_string n2 in
+  let env = Global.env () in 
+  let sigma = Evd.from_env env in
+  let (sigma, body) = Constrintern.interp_constr_evars env sigma e in
+  let (sigma, e1) = Constrintern.interp_constr_evars env sigma e1 in
+  let (sigma, e2) = Constrintern.interp_constr_evars env sigma e2 in
+  let typ = EConstr.to_constr sigma body in
+  let info = Declare.Info.make ~poly:false () in
+  let cinfo = Declare.CInfo.make ~name ~typ () in
+  let (t, types, ustate, obl_info) = Declare.Obls.prepare_obligation ~name ~types:None ~body sigma in
+  let tactic = tac_prove_by_crit ~evars ~constants ~e1 ~e2 ~r1 ~r2 ~crit ~l ~r in
+  let _, progress = Declare.Obls.add_definition ~pm:(Declare.OblState.empty) ~cinfo ~info ~uctx:ustate ~tactic obl_info in
+  match progress with
+  | Defined _ -> ()
+  | _ -> failwith ("Could not prove goal by crit: " ^ (Names.Id.to_string name))
 
 let cl_rewrite_clause_innermost ?(hyp:string = "H") (rewriter:Libnames.qualid) (left2right:bool) =
   let open Rewrite in
@@ -193,11 +196,13 @@ let tac_prove_by_reduction ~(rewriters : Libnames.qualid list)
     (if use_symmetry then Tactics.symmetry else Tacticals.tclIDTAC) <*>
     Auto.default_auto in
   let rec aux l2rs =
-    Tacticals.tclORELSE
-      (tactic_of l2rs)
-      (match Devutil.next_binls l2rs with
-      | None -> Tacticals.tclFAIL (Pp.str "Could not prove goal by reduction.")
-      | Some l2rs -> aux l2rs) in
+    Tacticals.tclIFCATCH
+      (Tacticals.tclPROGRESS (tactic_of l2rs))
+      (fun _ -> Tacticals.tclIDTAC)
+      (fun _ ->
+        match Devutil.next_binls l2rs with
+        | None -> Tacticals.tclFAIL (Pp.str "Could not prove goal by reduction.")
+        | Some l2rs -> aux l2rs) in
   aux (List.init (List.length rewriters + 1) (fun _ -> true))
 
 let prove_interreduce
